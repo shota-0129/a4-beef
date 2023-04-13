6e3876b2e659ca7599f4d92be73428f30c6e25c9
Mizusaki Shota <nrxg129@gmail.com>
2023-04-14 02:40:56 +0900

    feat: レスポンシブに対応

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,35 +1,35 @@
 import React, { useState } from 'react';
 import DeleteIcon from '@mui/icons-material/Delete';
 import SendIcon from '@mui/icons-material/Send';
+import Textarea from '@mui/joy/Textarea';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
-
-import './Popup.css';
 
 const Popup = (): React.ReactElement => {
   const [text, setText] = useState('');
 
-  const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleTextChange = (e: any) => {
     setText(e.target.value);
   };
 
   const handleSend = () => {
-    console.log('#####');
-    console.log(text);
+    alert(text);
   };
 
+  document.body.style.width = '25rem';
+  document.body.style.height = '20rem';
   return (
     <div className="popup-container">
       <h1>以下にどんなメールを書きたいか打ち込んでください</h1>
-      <input type="text" value={text} onChange={handleTextChange} />
-      {/* <Stack direction="row" spacing={2}>
+      <Textarea color="primary" minRows={3} onChange={handleTextChange} value={text} />
+      <Stack direction="row" spacing={2}>
         <Button variant="outlined" startIcon={<DeleteIcon />}>
           削除
         </Button>
-        <Button variant="contained" endIcon={<SendIcon />} onClick={handleSend}>
+        <Button variant="contained" onClick={handleSend} endIcon={<SendIcon />}>
           送信
         </Button>
-      </Stack> */}
+      </Stack>
     </div>
   );
 };
