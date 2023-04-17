ea252b3879ca83b08aa7b4f403974d4a7a026233
Mizusaki Shota <nrxg129@gmail.com>
2023-04-17 11:18:02 +0900

    feat: デザイン変更（レスポンシブに対応）

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import DeleteIcon from '@mui/icons-material/Delete';
 import SendIcon from '@mui/icons-material/Send';
 import Textarea from '@mui/joy/Textarea';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 
@@ -16,20 +17,29 @@ const Popup = (): React.ReactElement => {
     alert(text);
   };
 
-  document.body.style.width = '25rem';
-  document.body.style.height = '20rem';
+  const handleDelete = () => {
+    setText('');
+  };
   return (
-    <div className="popup-container">
-      <h1>以下にどんなメールを書きたいか打ち込んでください</h1>
-      <Textarea color="primary" minRows={3} onChange={handleTextChange} value={text} />
-      <Stack direction="row" spacing={2}>
-        <Button variant="outlined" startIcon={<DeleteIcon />}>
-          削除
-        </Button>
-        <Button variant="contained" onClick={handleSend} endIcon={<SendIcon />}>
-          送信
-        </Button>
-      </Stack>
+    <div>
+      <Box sx={{ m: 2 }}>
+        以下にどんなメールを書きたいか打ち込んでください
+        <Textarea
+          color="primary"
+          minRows={3}
+          onChange={handleTextChange}
+          value={text}
+          sx={{ my: 2 }}
+        />
+        <Stack direction="row" spacing={2} justifyContent="center">
+          <Button variant="outlined" onClick={handleDelete} startIcon={<DeleteIcon />}>
+            削除
+          </Button>
+          <Button variant="contained" onClick={handleSend} endIcon={<SendIcon />}>
+            送信
+          </Button>
+        </Stack>
+      </Box>
     </div>
   );
 };
