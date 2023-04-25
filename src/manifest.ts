34a4db92fa77cead461a6a9a9f44a61f03e7c90b
Mizusaki Shota <nrxg129@gmail.com>
2023-04-25 11:04:19 +0900

    feat: 細かい修正

--- src/manifest.ts
+++ src/manifest.ts
@@ -18,6 +18,15 @@ const manifest: ManifestV3Export = {
     page: 'src/options/options.html',
     open_in_tab: true,
   },
+  web_accessible_resources: [
+    {
+      resources: [
+        // this file is web accessible; it supports HMR b/c it's declared in `rollupOptions.input`
+        'src/welcome/welcome.html',
+      ],
+      matches: ['<all_urls>'],
+    },
+  ],
   action: {
     default_popup: 'src/popup/popup.html',
     default_icon: {
