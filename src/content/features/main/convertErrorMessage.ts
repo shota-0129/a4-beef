bef89071354145c724006ccec08e6ce93434d6cd
Shota Mizusaki <nrxg129@gmail.com>
2023-08-13 18:14:22 +0900

    feat: エラー文の追加

--- src/content/features/main/convertErrorMessage.ts
+++ src/content/features/main/convertErrorMessage.ts
@@ -18,6 +18,8 @@ export const convertErrorMessage = (errorText: string): string => {
       return 'ChatGPTでエラーが発生しました。\n もう一度お試し下さい';
     case 'APIError':
       return 'サーバーでエラーが発生しました。\n しばらく時間をおいてからもう一度お試しください。';
+    case 'import':
+      return 'メッセージを直接代入できませんでした。\n作成ボタンを押して、新しいメッセージを開いた状態で、お待ちください';
     default:
       return '不明なエラーが発生しました。\n しばらく時間をおいてからもう一度お試しください。';
   }
